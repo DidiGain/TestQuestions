75b213c152b037b804169ff410f02b78fda9d0e5
Didi <dignara@yandex.ru>
2019-05-04 19:05:12 +0300

    Initial Commit

---
+++ TestQuestions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestQuestions
+//
+//  Created by Di Gain on 04/05/2019.
+//  Copyright © 2019 Didi Gain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
